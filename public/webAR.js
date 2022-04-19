bd4afa21e02c749580aa02ef5da96d43e9c0488d
Miyakawa Takuya <omyakawa@gmail.com>
2022-04-19 12:59:26 +0900

    adjust judgment of GuChokiPa

--- public/webAR.js
+++ public/webAR.js
@@ -1,21 +1,43 @@
 //MediaPipeやOpenCVでの処理を記述
 let canvasElement;
 let canvasCtx;  //キャンバスコンテキストを使って絵を描く
-// let ell; //手の位置や傾きを楕円
+let ell; //手の位置や傾きを楕円
+let mul = 0;
+let ratio;
 let ratio_thumb;
 let ratio_index;
 let ratio_middle;
 let ratio_pinky;
 let mode_janken = false;
 let handState = 0;  //0 = 何でもない, 1=ぐー、２＝チョキ、３=パー
+//swingSE
+let posThumbs = [0, 0];  //[x座標, y座標]といったように格納していく
+let thumbPos_x = 0;
+let thumbPos_y = 0;
+let thumbPosPast_x = 0;
+let thumbPosPast_y = 0;
+let deltaPos_x = 0;
+let deltaPos_y = 0;
+let deltaPos = 0;
 let flag_forLeaveSpace = 0;
 let rightOrLeft = 0;  //1ならright 2ならleft
 let SE_flag = 0;
+//drawImage
+let guImage;
+let chokiImage;
+let paImage;
+//SE
+const SE_gu = new Audio('SE/gu.mp3');
+const SE_choki = new Audio('SE/choki.mp3');
+const SE_pa = new Audio('SE/pa.mp3');
+SE_gu.volume = 0.7;
 
 //初期化
 window.onload = function () {
     //画像の読み込み
-    beam = document.getElementById('beam');
+    guImage = document.getElementById('guImage');
+    chokiImage = document.getElementById('chokiImage');
+    paImage = document.getElementById('paImage');
     //ビデオ要素の取得
     let videoElement = document.getElementById('input_video');
     //表示用のCanvasを取得
@@ -38,9 +60,6 @@ window.onload = function () {
         useCpuInference: false, //M1 MacのSafariの場合は1  crhomかfirefoxでやる
     });
     //結果を処理する関数を登録
-    // console.log(hands);
-    // console.log(mode_janken);
-    // if (mode_janken) hands.onResults(recvResults);
     hands.onResults(recvResults);
 
     //カメラの初期化
@@ -74,13 +93,13 @@ function recvResults(results) {
         //見つけた手の数だけ処理を繰り返す
         for (const landmarks of results.multiHandLandmarks) {
             //骨格を描画(MediaPipeのライブラリ)  コメントアウトすれば表示せずに済む
-            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#040404', lineWidth: 1 });  //大きくすると線が太くなる
+            // drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#040404', lineWidth: 1 });  //大きくすると線が太くなる
             //関節を描画(MediaPipeのライブラリ)  コメントアウトすれば表示せずに済む
-            drawLandmarks(canvasCtx, landmarks, { color: '#000000', lineWidth: 1, radius: 2 });
+            // drawLandmarks(canvasCtx, landmarks, { color: '#000000', lineWidth: 1, radius: 2 });
             //orignal - openCVのファンクション landmarksの計算処理
             cvFunction(landmarks, width, height);
             //orignal - イメージを描画する関数
-            // drawLightSaber(); //自作関数
+            drawImage(); //グーチョキパーを描画する自作関数
         }
     }
     canvasCtx.restore();
@@ -94,8 +113,6 @@ function recvResults(results) {
         useCpuInference: false,  //M1 MacのSafariの場合は1  crhomかfirefoxでやる
     });
     //結果を処理する関数を登録
-    console.log("setOptions_to_2");
-    // hands.onResults(recv2Results);
     hands.onResults(recvResults);
     // }
 }
@@ -104,32 +121,32 @@ function recvResults(results) {
 function cvFunction(landmarks, width, height) {
     ////イメージを重ね合わせるための楕円の用意
     //手の関節を保持する配列
-    // let points = [];
+    let points = [];
     // //手のひらや親指の付け根付近以外の関節を取得
-    // for (var i = 0; i < 21; i++) {
-    //     //0~1で表現されたx,yを画像のサイズに変換
-    //     points.push(landmarks[i].x * width);
-    //     points.push(landmarks[i].y * height);
-    // }
+    for (var i = 2; i < 21; i++) {
+        //0~1で表現されたx,yを画像のサイズに変換
+        points.push(landmarks[i].x * width);
+        points.push(landmarks[i].y * height);
+    }
     //点の集まりをOpenCVで扱えるデータフォーマットmatに変換
-    // let mat = cv.matFromArray(points.length / 2, 1, cv.CV_32SC2, points);
+    let mat = cv.matFromArray(points.length / 2, 1, cv.CV_32SC2, points);
     //点の集まりにフィットする楕円を計算
-    // ell = cv.fitEllipse(mat);
+    ell = cv.fitEllipse(mat);
     //メモリの解放(変数定義するとメモリを消費しているので不要になったら消す)
-    // mat.delete();
+    mat.delete();
 
-    //親指の指先から人差指付け根までの距離 thumb
-    let dx = (landmarks[4].x - landmarks[5].x) * width;
-    let dy = (landmarks[4].y - landmarks[5].y) * height;
-    let thumb_d1 = Math.sqrt(dx * dx + dy * dy);
-    //人差し指付け根と小指付け根の距離
-    dx = (landmarks[5].x - landmarks[17].x) * width;
-    dy = (landmarks[5].y - landmarks[17].y) * height;
+    //親指の指先から手首付け根までの距離 thumb
+    let dx = (landmarks[4].x - landmarks[0].x) * width;
+    let dy = (landmarks[4].y - landmarks[0].y) * height;
     let thumb_d2 = Math.sqrt(dx * dx + dy * dy);
+    //親指の指先から薬指の第一関節の距離
+    dx = (landmarks[4].x - landmarks[15].x) * width;
+    dy = (landmarks[4].y - landmarks[15].y) * height;
+    let thumb_d1 = Math.sqrt(dx * dx + dy * dy);
     //親指の立ち具合
     ratio_thumb = thumb_d1 / thumb_d2;
     // console.log(ratio_thumb);
-    let close = 0.3;
+    let close = 0.5;
     let up = 0.6;
     ratio_thumb = (Math.max(close, Math.min(up, ratio_thumb)) - close) / (up - close); //0~1に正規化
     // console.log("thumb_up : " + ratio_thumb);
@@ -179,26 +196,32 @@ function cvFunction(landmarks, width, height) {
     // console.log("pinky_up : " + ratio_pinky);
 
     //グーチョキパー判定 //TODO 正面から見た時はしっかり判定してくれる状態だが、横向きになると恐らくratio_thumbが機能しておらず判定できてない
-    if (ratio_thumb == 1 && ratio_index == 1 && ratio_middle == 1 && ratio_pinky == 1) {
+    // if (ratio_thumb == 1 && ratio_index == 1 && ratio_middle == 1 && ratio_pinky == 1) {
+    if (ratio_index == 1 && ratio_middle == 1 && ratio_pinky == 1) {
+        if (handState != 3) wakeSE_pa();
         handState = 3;
-        console.log("handState:Pa - " + handState);
+        // thumb_deltaPos(landmarks[12]);
+        console.log("Pa : " + handState);
     } else if (ratio_thumb != 1 && ratio_index != 1 && ratio_middle != 1 && ratio_pinky != 1) {
+        if (handState != 1) wakeSE_gu();
         handState = 1;
-        console.log("handState:Gu - " + handState);
+        // thumb_deltaPos(landmarks[4]);
+        console.log("Gu : " + handState);
     } else if (ratio_thumb != 1 && ratio_index == 1 && ratio_middle == 1 && ratio_pinky != 1) {
+        if (handState != 2) wakeSE_choki();
         handState = 2;
-        console.log("handState:Choki - " + handState);
+        // thumb_deltaPos(landmarks[12]);
+        console.log("Choki : " + handState);
+    } else {
+        handState = 0; //リセット
     }
 }
-
-//ライトセイバーを表示
-function drawLightSaber() {  //画像、位置X、位置Y、横幅、縦幅
+//グーチョキパーを表示
+function drawImage() {  //画像、位置X、位置Y、横幅、縦幅
     //楕円の角度
     let angle = ell.angle;
     //ライトセイバーの向きを反転 openCVは第２.３象限でしか角度判定できない 
     if (angle < 90) { angle = angle - 180; }
-    //デフォルトサイズを元画像の２倍くらいにしたい  ratioをかけることで親指の立ち具合で大きさが変わるようになった
-    let mul = ratio * (ell.size.width * 1.5) / beam.width;
     //位置指定
     canvasCtx.translate(ell.center.x, ell.center.y);
     //角度指定
@@ -209,39 +232,20 @@ function drawLightSaber() {  //画像、位置X、位置Y、横幅、縦幅
         ell.size.width / 2.0, ell.size.height / 2.0,  //半径
         0, 0, 2 * Math.PI);    //角度と表示の開始・終了
     // canvasCtx.stroke();  //線で書くよ
-    // デフォルトサイズに倍数をかける
-    canvasCtx.scale(mul, mul);
-    if (nowPlaying) {
-        // canvasCtx.drawImage(beam, -beam.width / 2.0, 0, beam.width, beam.height); //画像の位置をあらかじめx方向に半分ずらす
-    } else if (nowPlaying) {
-        // canvasCtx.drawImage(beam_blue, -beam_blue.width / 2.0, 0, beam_blue.width, beam_blue.height);
+    if (handState == 1) {
+        mul = (ell.size.width * 3) / guImage.width;
+        canvasCtx.scale(mul, mul);
+        canvasCtx.drawImage(guImage, -guImage.width / 2.0, -guImage.height / 2.0, guImage.width, guImage.height);
+    } else if (handState == 2) {
+        mul = (ell.size.width * 2.5) / chokiImage.width;
+        canvasCtx.scale(mul, mul);
+        canvasCtx.drawImage(chokiImage, -chokiImage.width / 2.0, -chokiImage.height / 2.0, chokiImage.width, chokiImage.height);
+    } else if (handState == 3) {
+        mul = (ell.size.width * 3.5) / paImage.width;
+        canvasCtx.scale(mul, mul);
+        canvasCtx.drawImage(paImage, -paImage.width / 2.0, -paImage.height / 2.0, paImage.width, paImage.height);
     }
 }
-
-//かめはめ波を表示
-// function drawKamehameha() {  //画像、位置X、位置Y、横幅、縦幅
-//     //楕円の角度
-//     // let angle = ell_rect.angle;
-//     let angle = ell_rect.angle + 90;
-//     //ライトセイバーの向きを反転 openCVは第２.３象限でしか角度判定できない 
-//     if (angle < 90) { angle = angle + 180; }
-//     //デフォルトサイズを元画像の２倍くらいにしたい  ratioをかけることで親指の立ち具合で大きさが変わるようになった
-//     let mul = 3.0 * (ell_rect.size.width * 1.5) / kameha.width;
-//     //位置指定
-//     canvasCtx.translate(ell_rect.center.x, ell_rect.center.y);
-//     //角度指定
-//     canvasCtx.rotate(angle * Math.PI / 180.0); //openCVの角度は°でcanvasはラジアン
-//     //楕円を描画
-//     canvasCtx.beginPath();    //複数の点をつなぐ線を書くよの宣言
-//     canvasCtx.ellipse(0, 0,   //位置 楕円そのものでは位置指定せず、全体のオブジェクトに対してtranslate()で指定する
-//         ell_rect.size.width / 2.0, ell_rect.size.height / 2.0,  //半径
-//         0, 90, 2 * Math.PI);    //角度と表示の開始・終了
-//     // ellipse(中心のx座標, 中心のy座標, x方向の半径, y方向の半径,傾き, 開始角度, 終了角度, [回転方向]);
-//     // canvasCtx.stroke();  //線で書くよ
-//     //デフォルトサイズに倍数をかける
-//     canvasCtx.scale(mul, mul);
-//     canvasCtx.drawImage(kameha, -kameha.width / 2.0, -kameha.width / 12.0, kameha.width, kameha.height); //画像の位置をあらかじめx方向に半分ずらす
-// }
 
 //親指が立っている間に親指のxy座標を拾って、差分を取って速度を計算する
 function thumb_deltaPos(landmark_4) {
@@ -253,17 +257,17 @@ function thumb_deltaPos(landmark_4) {
     deltaPos_y = thumbPos_y - thumbPosPast_y;
     deltaPos = Math.sqrt(deltaPos_x * deltaPos_x + deltaPos_y * deltaPos_y) * 100;
     // console.log(deltaPos);
-    let a = deltaPos_x * 100;
-    console.log(a);
+    // let a = deltaPos_x * 100;
+    let a = deltaPos_y * 100;
+    // console.log(a);
     if (a >= 0) {
         rightOrLeft = 1;
     } else if (a < 0) {
         rightOrLeft = 2;
     }
-    // console.log(rightOrLeft);
     //移動距離が一定量より大きかったら効果音を出す
     if (deltaPos >= 5) {
-        swingSaber_SE();
+        swingSE();
     }
     //リセット
     thumbPosPast_x = thumbPos_x;
@@ -272,13 +276,25 @@ function thumb_deltaPos(landmark_4) {
 }
 
 /////効果音
-//ライトセイバー振った時の効果音
-function swingSaber_SE() {
+function wakeSE_gu() {
+    // if (SE_flag == 0) {
+    SE_gu.play();
+    // SE_flag++;
+    // }
+}
+function wakeSE_choki() {
+    SE_choki.play();
+}
+function wakeSE_pa() {
+    SE_pa.play();
+}
+
+function swingSE() {
     if (rightOrLeft == 1) {
-        // console.log("swingRight");
-        SE_right.play();
+        console.log("L");
+        // SE_right.play();
     } else if (rightOrLeft == 2) {
-        // console.log("swingLeft");
-        SE_left.play();
+        console.log("R");
+        // SE_left.play();
     }
 }
